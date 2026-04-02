7f64c2b21d5d281d3ada7bba7dc418c4f5da4a4f
Estevan Bravim <estevanbravim2@gmail.com>
2026-04-02 17:17:17 -0300

    32142

---
+++ EX019/script.js
@@ -0,0 +1,20 @@
+function tabuada(){
+var num=document.getElementById('txtn')
+var tab= document.getElementById('seltab')
+if(num.value.length ==0){
+ window.alert('Por favor, digite um numero!')}
+else{
+   
+    var n =Number(num.value)}
+    let c = 1
+    tab.innerHTML=''
+    while (c <=100){
+        let item =document.createElement('option')
+        item.text =`${n} x ${c} = ${n*c}`
+        item.value =`tab${c}`
+        tab.appendChild(item)
+        c++
+    }
+
+
+}
---
+++ EX020/vetores.js
@@ -0,0 +1,4 @@
+var num=[2,3,9,8,3,2,2]
+for(let pos in num){
+    console.log(`${num[pos]}`)
+}
